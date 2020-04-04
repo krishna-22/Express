7bfce2c6470a85ff6388be5d03f0f8725a35d820
krishna-22 <goginenisaikiran31677@gmail.com>
2020-04-04 12:51:42 +0530

    secondary

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express= require('express')
 const http = require('http')
 const hostname= 'localhost'
+const morgan = require('morgan')
 const port=3000
 const app = express()
+app.use(morgan('dev'))
+app.use(express.static(__dirname+'/public'))
 app.use((req,res,next)=>{
-    console.log(req.headers)
+   
     res.statusCode=200
     res.setHeader('content-Type','text/html')
     res.end('<html><body>Hello world</body></html>')
