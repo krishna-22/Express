0211663986a9445535fb3ceb6d2626a44e1a0a9b
krishna-22 <goginenisaikiran31677@gmail.com>
2020-04-04 14:05:30 +0530

    express router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 //use postman tool to undetstand the topics with localhost urls
+// type index.html about.html nothing in url bars
 const express= require('express')
 const http = require('http')
 const hostname= 'localhost'
+const dishRouter =require('./routes/dishRouter')
 const morgan = require('morgan')
 const port=3000
 const bodyParser =require('body-parser')
@@ -9,53 +11,12 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(express.static(__dirname+'/public'))
-app.all('/dishes',(req,res,next)=>{
-    res.statusCode=200
-    res.setHeader('content-Type','text/plain')
-    next()
-})
-app.get('/dishes',(req,res)=>
-{
-    res.end('will send all the dishes to you')
-})
 
-app.get('/dishes/:dishid',(req,res)=>
-{
-    res.end('will send you details of dish '+req.params.dishid)
-})
+app.use('/dishes',dishRouter)
 
-app.post('/dishes',(req,res)=>
-{
-    res.end('i will add the dish:'+ req.body.name+ 'with details'+req.body.description)
-}) 
 
-app.post('/dishes/:dishid',(req,res)=>
-{
-    res.statusCode=403
-    res.end('post is not supported on individual dishes such as'+req.params.dishid)
-}) 
 
-app.put('/dishes',(req,res)=>
-{
-    res.statusCode=403
-    res.end('put operation is not supported on dishes')
-}) 
 
-app.put('/dishes/:dishid',(req,res)=>
-{
-    res.write('updating dish '+req.params.dishid)
-    res.end('updating the dish '+req.body.name+'with details '+req.body.description)
-}) 
-
-app.delete('/dishes',(req,res)=>
-{
-    res.end('deleting all the dishes !')
-}) 
-
-app.delete('/dishes/:dishid',(req,res)=>
-{
-    res.end('deleting the dish!'+ req.params/dishid)
-}) 
 
 app.use((req,res,next)=>{
    
---
+++ routes/dishRouter.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const dishRouter = express.Router()
+
+dishRouter.use(bodyparser.json())
+
+dishRouter.route('/')
+.all((req,res,next)=>{
+    res.statusCode=200
+    res.setHeader('content-Type','text/plain')
+    next()
+})
+
+.get((req,res)=>
+{
+    res.end('will send all the dishes to you')
+})
+
+.post((req,res)=>
+{
+    res.end('i will add the dish:'+ req.body.name+ 'with details'+req.body.description)
+}) 
+
+.put((req,res)=>
+{
+    res.statusCode=403
+    res.end('put operation is not supported on dishes')
+}) 
+
+.delete((req,res)=>
+{
+    res.end('deleting all the dishes !')
+}) ;
+
+module.exports= dishRouter
